94611bdfb8d55f64fb5994d023d8ea17e351e4ba
noorqidam <noorqidam@gmail.com>
2020-11-24 20:30:04 +0700

    add plugins unique-slug and deleted blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,27 +1,33 @@
-const { json } = require('body-parser');
-const mongoose = require('mongoose');
-const Blog = mongoose.model('Blog');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const uniqueSlug = require("unique-slug");
+const Blog = mongoose.model("Blog");
 
 exports.getBlogs = async (req, res) => {
-  const blogs = await Blog.find({status: 'published'}).sort({createdAt: -1});
+  const blogs = await Blog.find({ status: "published" }).sort({
+    createdAt: -1,
+  });
   return res.json(blogs);
-}
+};
 
-exports.getBlogsByUser = async (req,res) => {
+exports.getBlogsByUser = async (req, res) => {
   const userId = req.user.sub;
-  const blogs = await Blog.find({userId});
+  const blogs = await Blog.find({
+    userId,
+    status: { $in: ["draft", "published"] },
+  });
   return res.json(blogs);
-}
+};
 
 exports.getBlogById = async (req, res) => {
   const blog = await Blog.findById(req.params.id);
   return res.json(blog);
-}
+};
 
 exports.getBlogBySlug = async (req, res) => {
-  const blog = await Blog.findOne({slug: req.params.slug})
+  const blog = await Blog.findOne({ slug: req.params.slug });
   return res.json(blog);
-}
+};
 
 exports.createBlog = async (req, res) => {
   const blogData = req.body;
@@ -31,30 +37,53 @@ exports.createBlog = async (req, res) => {
   try {
     const createdBlog = await blog.save();
     return res.json(createdBlog);
-  } catch(e) {
+  } catch (e) {
     return res.status(422).send(e.message);
   }
-}
+};
 
-exports.updateBlog = async (req,res) => {
-  const { body, params: {id}} = req;
+const _saveBlog = async (blog) => {
+  try {
+    const createdBlog = await blog.save();
+    return createdBlog;
+  } catch (e) {
+    if (e.code === 11000 && e.keyPattern && e.keyPattern.slug) {
+      blog.slug += `${uniqueSlug()}`;
+      return _saveBlog(blog);
+    }
+    throw e;
+  }
+};
+
+exports.updateBlog = async (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
 
   Blog.findById(id, async (err, blog) => {
     if (err) {
-      return res.status(422).send(err.message)
+      return res.status(422).send(err.message);
     }
 
-    // TODO: Check if user is publishing blog 
+    // TODO: Check if user is publishing blog
     // and if user is publishing then create SLUG
+
+    if (body.status && body.status === "published" && !blog.slug) {
+      blog.slug = slugify(blog.title, {
+        replacement: "-",
+        lower: true,
+      });
+    }
 
     blog.set(body);
     blog.updateAt = new Date();
 
     try {
-      const updateBlog = await blog.save();
+      const updateBlog = await _saveBlog(blog);
       return res.json(updateBlog);
     } catch (err) {
-      return res.status(422).send(err.message)
+      return res.status(422).send(err.message);
     }
   });
-}
+};
